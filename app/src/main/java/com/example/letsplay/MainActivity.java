44a3a8fd4c0be2c3abbe38a3c39dde875d7dcc5d
harsh8999 <46605288+harsh8999@users.noreply.github.com>
2019-03-06 18:39:39 +0530

    initial commit

---
+++ app/src/main/java/com/example/letsplay/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.letsplay;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView postList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_Layout);
+
+        RecyclerView postList = (RecyclerView) findViewById(R.id.postList);
+
+
+    }
+}
