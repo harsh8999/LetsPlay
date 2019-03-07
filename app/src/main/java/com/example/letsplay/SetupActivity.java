cb6a4586adcefadd1941100f00abad6173051c7e
harsh8999 <46605288+harsh8999@users.noreply.github.com>
2019-03-07 21:12:39 +0530

    Setup Updated

--- app/src/main/java/com/example/letsplay/SetupActivity.java
+++ app/src/main/java/com/example/letsplay/SetupActivity.java
@@ -2,12 +2,26 @@ package com.example.letsplay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SetupActivity extends AppCompatActivity {
+
+    private EditText UserName , FullName , CountryName;
+    private Button SaveInformationButton;
+    private CircleImageView ProfileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        UserName = (EditText) findViewById(R.id.setup_username);
+        FullName = (EditText) findViewById(R.id.setup_fullname);
+        CountryName = (EditText) findViewById(R.id.setup_country);
+        SaveInformationButton = (Button) findViewById(R.id.setup_information_button);
+        ProfileImage = (CircleImageView) findViewById(R.id.setup_profile_image);
     }
 }
