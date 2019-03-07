cf6602c4b0f4f8412ad9e4fab29ca0d47a6668e1
Kanisha04 <kanishadesai04@gmail.com>
2019-03-07 21:38:46 +0530

    navigation_menu.xml modified

--- app/src/main/java/com/example/letsplay/MainActivity.java
+++ app/src/main/java/com/example/letsplay/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.database.annotations.NotNull;
 
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView postList = (RecyclerView) findViewById(R.id.postList);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawable_Layout);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 UserMenuSelector(item);
@@ -37,8 +40,26 @@ public class MainActivity extends AppCompatActivity {
         {
             switch (item.getItemId())
             {
+                case R.id.nav_profile:
+                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                    break;
 
+                case R.id.nav_home:
+                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.nav_messages:
+                    Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.nav_settings:
+                    Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.nav_Logout:
+                    Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                    break;
             }
+
         }
 
     }
